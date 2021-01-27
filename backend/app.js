7c0dd1bbcdeab63e4f4b8ceb9b7410bc52a6c5ef
Anton Shubin <antonsh2005@yandex.ru>
2021-01-27 12:40:46 +0300

    fix: error-json

--- backend/app.js
+++ backend/app.js
@@ -64,7 +64,7 @@ app.use(() => {
 
 app.use((err, req, res, next) => {
   if (err.status) {
-    res.status(err.status).send(err.message);
+    res.status(err.status).send({ message: err.message });
     return;
   }
   res
